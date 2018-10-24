d56fbf0172eff5f7c52390bc0668dba2caf253ba
Pecora <pecorafatec@gmail.com>
2018-10-24 00:38:29 -0300

    i fall apart

--- domains/stage.890m.com/public_html/Site/empresa/gerenciar_vagas.php
+++ domains/stage.890m.com/public_html/Site/empresa/gerenciar_vagas.php
@@ -1,42 +1,5 @@
 <!DOCTYPE html>
-<?php 
-                            if(!isset($_COOKIE['email'])){
-                                header("Location:../Site/home.php");
-                            } ?>
-<html>
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="theme.css" type="text/css"> </head>
-
-<body>
-  <nav class="navbar navbar-expand-md bg-primary navbar-dark bg-gradient">
-    <div class="container">
-      <a class="navbar-brand" href="home2.php">
-        <i class="fa d-inline fa-lg fa-cloud"></i>
-        <b class="text-uppercase">Stage</b>
-      </a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="perfil.php">Bem Vindo:&nbsp;
-              <i class="fa d-inline fa-lg fa-user-circle-o"></i> 
-                                                                    <?php 
-                                                                    echo $_COOKIE['email'];
-                                                                    ?></a>
-          </li>
-        </ul>
-        <a href="../back_end/disconnect_user.php">
-          <button type="button" class="btn btn-primary"> &nbsp;Logoff</button>
-        </a>
-      </div>
-    </div>
-  </nav>
+<?php include_once 'navbar_empresa.php'; ?>
   <div class="">
     <div class="container">
       <div class="row">
