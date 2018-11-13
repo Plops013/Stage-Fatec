63d14476a4489e49baa483c9298f29482a5c797b
Pecora <pecorafatec@gmail.com>
2018-11-13 07:18:10 -0200

    bolsonaro

--- domains/stage.890m.com/public_html/Site/candidato/teste.php
+++ domains/stage.890m.com/public_html/Site/candidato/teste.php
@@ -1,8 +1,32 @@
 <?php
+if (!isset($_COOKIE['email'])) {
+    header("Location:../Site/home.php");
+} else {
+    include_once '../../back_end/Vaga.php';
+    include_once '../../back_end/Empresa.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $email = $_COOKIE['email'];
+    $cnpj = $_COOKIE['cnpj'];
+    $v = new Vaga();
+    $result = $v->getVagas();
+}
+?>
+<?php include_once 'navbar_candidato.php'; ?>
 
+<div>
+    <h1> Pergunta </h1>
+    <form>
+        <input type="radio" value="1">
+        <input type="radio" value="2">
+        <input type="radio" value="3">
+        <input type="radio" value="4">
+        <input type="radio" value="5">
+    </form>
+</div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- domains/stage.890m.com/public_html/hash/home.php
+++ domains/stage.890m.com/public_html/hash/home.php
@@ -1,9 +1,9 @@
 <?php
-    $i = $_POST['index'] + 1;
-    if($_POST['reset'] != null){
-        $i = 0;
-        $_POST['texto'] = null;
-    }
+$i = $_POST['index'] + 1;
+if ($_POST['reset'] != null) {
+    $i = 0;
+    $_POST['texto'] = null;
+}
 ?>
 <html>
     <head>
@@ -16,47 +16,89 @@
 
     <body>
         <br/>
-        <div class="container mx-auto text-center">
-            <h1 class="display-3">Criptografia HASH em PHP</h1>
-            <form method="POST" action="home.php">
-                <div class="form-group row">
-                    <label class="col-sm-1 col-form-label">Texto</label>
-                    <div class="col-sm-9">
-                        <input class="form-control" name="texto" type="text" name="search">
-                        <input class="form-control" name="index" type="hidden" value="<?php echo $i ?>" name="search">
+        <div class="mx-auto">
+            <div class="container mx-auto text-center">
+                <h1 class="display-3">Criptografia HASH em PHP</h1>
+                <form method="POST" action="home.php">
+                    <div class="form-group row">
+                        <label class="col-sm-1 col-form-label">Texto</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" name="texto" type="text" name="search">
+                            <input class="form-control" name="index" type="hidden" value="<?php echo $i ?>" name="search">
+                        </div>
+                        <div class="col-sm-1">
+                            <input class="mx-auto text-center btn btn-success" type="submit" value="Enviar">
+                        </div>
+                        <div class="col-sm-1">
+                            <input class="mx-auto text-center btn btn-danger" name=reset type="submit" value="Resetar">
+                        </div>
                     </div>
-                    <div class="col-sm-1">
-                        <input class="mx-auto text-center btn btn-success" type="submit" value="Enviar">
-                    </div>
-                    <div class="col-sm-1">
-                        <input class="mx-auto text-center btn btn-danger" name=reset type="submit" value="Resetar">
-                    </div>
+                    <?php
+                    $dado[$i] = $_POST['texto'];
+                    if ($dado[$i] != null) {
+                        ?>
+                        </tr>
                 </div>
-                <?php
-                $dado[$i] = $_POST['texto'];
-                if ($dado[$i] != null){
-                ?>
-                </tr>
-        </div>
-        <div class="container mx-auto text-center">
-            <table class="table table-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Sem Criptografia</th>
-                    <th scope="col">MD5</th>
-                    <th scope="col">SHA1</th>
-                    <th scope="col">Crypt</th>
-                </tr>
-                <tr>
-                    <td class="text-center"><?php echo $i ?></td>
-                    <td class="text-center"><?php echo $dado[$i] ?></td>
-                    <td class="text-center"><?php echo md5($dado[$i]) ?></td>
-                    <td class="text-center"><?php echo sha1($dado[$i]) ?></td>
-                    <td class="text-center"><?php echo crypt($dado[$i]) ?></td>
-                </tr>
-                </form>
-            </table>
+                <div class="jumbotron bg-dark mx-auto text-center">
+                    <p></p>
+                    <table class="table table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Sem Criptografia</th>
+                            <th scope="col">MD5 (32 Digitos)</th>
+                            <th scope="col">SHA1 (40 Digitos)</th>
+                            <th scope="col">Crypt (34 Digitos)</th>
+                        </tr>
+                        <tr>
+                            <td class="text-center"><?php echo $i ?></td>
+                            <td class="text-center"><?php echo $dado[$i] ?></td>
+                            <td class="text-center"><?php echo md5($dado[$i]) ?></td>
+                            <td class="text-center"><?php echo sha1($dado[$i]) ?></td>
+                            <td class="text-center"><?php echo crypt($dado[$i]) ?></td>
+                        </tr>
+                        </form>
+                    </table>
                 <?php } ?>
+            </div>
+            <div class="jumbotron mx-auto text-center bg-dark text-white">
+                <h1 class="display-4">Hash</h1>
+                <p>
+                    Um hash é uma sequência de bits geradas por um algoritmo de dispersão, em geral representada em base hexadecimal, que permite a visualização em letras e números (0 a 9 e A a F). O conceito teórico diz que "hash é a transformação de uma grande quantidade de dados em uma pequena quantidade de informações"
+                </p>
+                <p>
+                    Essa sequência busca identificar um arquivo ou informação unicamente. É um método para transformar dados de tal forma que o resultado seja (quase) exclusivo. Além disso, funções usadas em criptografia garantem que não é possível a partir de um valor de hash retornar à informação original.
+                </p>
+                <br/>
+                <h1 class="display-4">Utilidades</h1>
+                <p>
+                    Assinatura Digital, Boleto Bancario (Com 1 digito Verificador), Armazenamento de senhas no banco de dados, Armazenamento de cartão no banco de dados
+                </p> 
+                <br/>
+                <h1 class="display-4">Funçoes em Linguagens Diferentes</h1>
+                <h2>PHP</h2>
+                <p>
+                    Crypt(<span class="text-warning">string</span>); <br/>
+                    md5(<span class="text-warning">string</span>); <br/>
+                    sha1(<span class="text-warning">string</span>); <br/>
+                    Fonte : <a href="http://php.net/manual/pt_BR/function.hash.php" target="_blank">Clique aqui</a>
+                </p>
+                <h2>Java</h2>
+                <p>
+                    final MessageDigest md = MessageDigest.getInstance("MD5"); <br/>
+                    final byte[] hash = md.digest(toBeHashed.getBytes()); <br/>
+                    return hash; <br/>
+                    <br/>
+                    final MessageDigest md = MessageDigest.getInstance("MD2"); <br/>
+                    final MessageDigest md = MessageDigest.getInstance("SHA-1");<br/>
+                    final MessageDigest md = MessageDigest.getInstance("SHA-256");<br/>
+                    final MessageDigest md = MessageDigest.getInstance("SHA-512");<br/>
+                    Fonte : <a href="http://www.ricardogiaviti.com.br/2016/10/gerando-hash-com-java/" target="_blank">Clique aqui</a>
+                </p>
+                <br/>
+                <h2>C</h2>
+                <p>hash(<span class="text-warning">dado</span>);</p>
+                Fonte : <a href="https://www.vivaolinux.com.br/script/Funcao-de-hash-simples-em-C" target="_blank">Clique aqui</a>
+            </div>
         </div>
     </body>
 </html>
